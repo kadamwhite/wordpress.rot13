31338e4c3ba7331698ba3b7545852bf94722123d
K.Adam White <kadamwhite@gmail.com>
2012-04-27 23:00:47 -0400

    Now plugin correctly preserves any HTML within the shortcode

--- rot13-shortcode.php
+++ rot13-shortcode.php
@@ -24,18 +24,35 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-add_shortcode( 'rot13', 'kaw_rot13_shortcode' );
+add_shortcode('rot13', 'kaw_rot13_shortcode');
 
-function kaw_rot13_shortcode( $atts, $content ){
-	$attributes = shortcode_atts( array(
-		'showlink' => false,
-		'animate' => false
-	), $atts );
-	// $atts ideas:
-	// - Whether to omit the "rot13.com" link next to the text
-	// - Whether to animate the decoding of the string one letter at a time
-	// - Whether to include the JavaScript decoding function at all
-	return '<span class="rot13-text">'.str_rot13($content).'</span>';
+function kaw_rot13_shortcode($atts, $content)
+{
+    extract(shortcode_atts(array(
+        'showlink' => false,
+        'animate' => false
+    ), $atts));
+    // $atts ideas:
+    // - Whether to omit the "rot13.com" link next to the text
+    // - Whether to animate the decoding of the string one letter at a time
+    // - Whether to include the JavaScript decoding function at all
+
+    $html_tag_pattern = '/(<[^>]*>)/';
+    $strings = preg_split($html_tag_pattern, $content, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $response = '<span class="rot13">';
+    foreach ($strings as $text) {
+        // Don't encode HTML tags
+        if (preg_match($html_tag_pattern, $text)) {
+            $response .= $text;
+        } else {
+            $response .= str_rot13($text);
+        }
+    }
+    if ($showlink) {
+        $response .= ' (<a href="http://www.rot13.com">rot13.com</a>) ';
+    }
+    $response .= '</span>';
+    return $response;
 }
 
 ?>
