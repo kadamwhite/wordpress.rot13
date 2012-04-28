c9413399911bb776155adf95baab8db83262575c
K.Adam White <kadamwhite@gmail.com>
2012-04-27 22:36:44 -0400

    Most basic version: Plugin does nothing but rot13-encode the tag contents

---
+++ rot13-shortcode.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Rot13 Spoiler Obfuscator
+Plugin URI: (in progress)
+Description: <a href="http://rot13.com/">Rot13</a> encoding is a quick and easy way to obfuscate or disguise text on your website. By rotating (rot) each letter 13 characters forward in the alphabet, you can easily <a href="http://gameshelf.jmac.org/2012/04/lets-use-rot13-for-game-spoilers/">discuss spoilers</a> without bothering readers who don't want to be spoiled. Just enclose the text you want to hide with the [rot13]shortcode[/rot13].
+Version: 0.1.0
+Author: K.Adam White
+Author URI: http://www.kadamwhite.com
+License: GPLv2 or later
+*/
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+add_shortcode( 'rot13', 'kaw_rot13_shortcode' );
+
+function kaw_rot13_shortcode( $atts, $content ){
+	$attributes = shortcode_atts( array(
+		'showlink' => false,
+		'animate' => false
+	), $atts );
+	// $atts ideas:
+	// - Whether to omit the "rot13.com" link next to the text
+	// - Whether to animate the decoding of the string one letter at a time
+	// - Whether to include the JavaScript decoding function at all
+	return '<span class="rot13-text">'.str_rot13($content).'</span>';
+}
+
+?>
